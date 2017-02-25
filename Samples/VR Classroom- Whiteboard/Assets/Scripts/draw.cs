89bc4adc923b5916792470b0c6e76dc5ba2776b3
Luis Calona <lcalona@ucsd.edu>
2017-02-24 18:17:56 -0800

    Recommit

---
+++ Samples/VR Classroom- Whiteboard/Assets/Scripts/draw.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class draw : MonoBehaviour {
+
+    public Material material;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        Texture2D texture = (Texture2D)material.GetTexture("white");
+
+        Vector3 direction = GvrController.Orientation * Vector3.forward;
+
+        Ray ray = new Ray(transform.position, direction);
+        RaycastHit raycastHit;
+
+        if (Physics.Raycast(ray, out raycastHit, 200f) && raycastHit.transform.gameObject.name.Equals("Whiteboard"))
+        {
+            Vector3 hitPosition = raycastHit.point;
+            int x = (int)hitPosition.x;
+            int y = (int)hitPosition.y;
+            
+            if (GvrController.IsTouching)
+            {
+                for(int i = -10; i <= 10; i++)
+                {
+                    for(int j = -10; j <= 10; j++)
+                    {
+                        texture.SetPixel(x + i, y + j, Color.black);
+                    }
+                    
+                }
+                
+                texture.Apply();
+                material.SetTexture("drawing", texture);
+            }
+        }
+    }
+}
